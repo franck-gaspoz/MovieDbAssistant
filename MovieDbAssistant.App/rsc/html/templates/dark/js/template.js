58b808ca5dfd257c6790b44d16bfeae4b6849c9d
franck gaspoz <franck.gaspoz@gmail.com>
2024-09-29 19:09:56 +0200

    activate template javascript for detail page

--- MovieDbAssistant.App/rsc/html/templates/dark/js/template.js
+++ MovieDbAssistant.App/rsc/html/templates/dark/js/template.js
@@ -1,16 +1,17 @@
-﻿/**
+﻿const ID_Model_Item = 'ItemModel';
+const ID_Model_Class_Movie_List = 'movie-list';
+
+/**
  * front sode template engine
  * @class
 */
 class Template {
 
-    /** @param {Object[]} data movies list */
-    buildItems(data) {
-
-        for (var e in data) {
-            console.debug(e);
-        }
-    }
+    /**
+     * @typedef MoviesModel movies model
+     * @type {object}
+     * @property {MovieModel[]} Movies movies models
+     */
 
     /**
      * @typedef MovieModel movie model
@@ -29,7 +30,7 @@ class Template {
      * @property {string} Vote vote
      * @property {string} Director director
      * @property {string[]} Writers writers
-     * @property {string[]} Starts stars 
+     * @property {string[]} Stars stars 
      * @property {ActorModel[]} Actors actors 
      * @property {string} Anecdotes anecdotes
      * @property {string} MinPicUrl min pic url
@@ -40,6 +41,33 @@ class Template {
      * @property {string[]} PicsSizes pics sizes 
      */
 
+    props = {
+        "Interests": (o, value) => o.hseps(value),
+        "Stars": (o, value) => o.hseps(value),
+        "Actors": (o, value) => o.hseps(value, x => o.actorSimple(x)),
+        "PicsUrls": null,
+        "PicsSizes": null
+    };
+
+    hseps(t, tr) {
+        if (!tr) tr = x => x;
+        return t.map(x => tr(x))
+            .join(this.hsep())
+    }
+
+    hsep() {
+        return '<span class="hsep"></span>';
+    }
+
+    /**
+     * actor simple html
+     * @param {ActorModel} actor Actor
+     * @returns actor simple html
+     */
+    actorSimple(actor) {
+        return actor.Actor
+    }
+
     /**
      * @typedef ActorModel actor model
      * @property {string} Actor actor
@@ -47,8 +75,50 @@ class Template {
      * @property {string[]} Characters characters
      */
 
-    /**@param {MovieModel} data movie object */
+    /** @param {MoviesModel} data movies set */
+    buildItems(data) {
+
+        for (var e in data) {
+            //console.debug(e);
+        }
+    }
+
+    /** @param {MovieModel} data movie */
+    buildDetails(data) {
+        var $src = $('.' + ID_Model_Class_Movie_List)
+        var html = $src[0].outerHTML
+        html = this.parseVars(html, data)
+        $src.html(html)
+        console.debug(html)
+    }
+
+    /**@param {MovieModel} data movie */
     addItem(data) {
-        data.string
+
+    }
+
+    /**
+     * 
+     * @param {string} tpl html source
+     * @param {MovieModel} data 
+     */
+    parseVars(tpl, data) {
+        for (var p in data) {
+            tpl = tpl.replaceAll(
+                this.getVar(p),
+                this.props[p] ?
+                    this.props[p](this, data[p])
+                    : data[p]
+            )
+        }
+        return tpl
+    }
+
+    firstLower(txt) {
+        return txt.charAt(0).toLowerCase() + txt.slice(1);
+    }
+
+    getVar(name) {
+        return '{{' + this.firstLower(name) + '}}';
     }
 }
