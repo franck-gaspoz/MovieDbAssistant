becb969369cd3705e89f8afe616a9a085dbccc87
franck gaspoz <franck.gaspoz@gmail.com>
2024-10-15 19:18:23 +0200

    adding tpl included props

--- MovieDbAssistant.Dmn/Components/Builders/Templates/TemplateBuilder.Parts.cs
+++ MovieDbAssistant.Dmn/Components/Builders/Templates/TemplateBuilder.Parts.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml.Linq;
-
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 using MovieDbAssistant.Dmn.Configuration.Extensions;
 using MovieDbAssistant.Lib.Components.Logger;
@@ -24,16 +22,16 @@ public partial class TemplateBuilder
     public const string Include_Part_Block_Begin = "--";
 
     public const string Parts_File_Extensions = ".tpl.html";
-    
+
     public const int Index_NoNext = -1;
 
     string IncludeParts(string tpl, Dictionary<string, object?>? props = null)
     {
-        tpl = ParseIncludes(tpl,props ?? []);
+        tpl = ParseIncludes(tpl, props ?? []);
         return tpl;
     }
 
-    string ParseIncludes(string tpl,Dictionary<string, object?> props)
+    string ParseIncludes(string tpl, Dictionary<string, object?> props)
     {
         var nextPos = 0;
         while (nextPos > Index_NoNext)
@@ -41,27 +39,27 @@ public partial class TemplateBuilder
         return tpl;
     }
 
-    (string content,int nextPos) ParseNextInclude(
+    (string content, int nextPos) ParseNextInclude(
         string tpl,
         Dictionary<string, object?> props,
-        int startPos=0)
+        int startPos = 0)
     {
         var x = tpl.IndexOf(Include_Part_Prefix, startPos);
         if (x < 0) return (tpl, Index_NoNext);
-        var y = tpl.IndexOf(Include_Part_Postfix,x);
+        var y = tpl.IndexOf(Include_Part_Postfix, x);
         if (y < 0) return (tpl, Index_NoNext);
         var nextY = y + Include_Part_Postfix.Length;
         var a = x + Include_Part_Postfix.Length;
         var b = y - 1;
-        var name = tpl.Substring(a,b-a+1);
+        var name = tpl.Substring(a, b - a + 1);
 
-        (tpl, name, var tpIlProps) = ParseIncludeProps(tpl,name,x);
+        (tpl, name, var tpIlProps) = ParseIncludeProps(tpl, name, y + Include_Part_Postfix.Length);
         (name, var tplProps) = ExtractProps(name);
         tplProps.MergeInto(props);
 
         var partFile = name + Parts_File_Extensions;
         var file = GetTemplateFile(partFile);
-        if (file==null) return (tpl, nextY);
+        if (file == null) return (tpl, nextY);
 
         var partContent = File.ReadAllText(file);
 
@@ -77,34 +75,38 @@ public partial class TemplateBuilder
         return (tpl, nextY);
     }
 
+    /// <summary>parse props included in tpl</summary>
+    /// <param name="x">first index after the name with tags</param>
     (string tpl, string name, Dictionary<string, object?> props) ParseIncludeProps(
-        string tpl,string name,int x)
+        string tpl, string name, int x)
     {
         var props = new Dictionary<string, object?>();
         var def = (tpl, name, props);
 
         if (!name.EndsWith(Include_Part_Block_Begin)) return def;
-        
+
+        var a = x + name.Length;
         name = name[..^(Include_Part_Block_Begin.Length)];
         var closeName = Include_Part_Block_Begin + name;
-        var a = x + name.Length;
         var b = tpl.IndexOf(closeName, a);
         var y = b;
         if (y < 0)
         {
-            _logger.LogWarning(this, "parse include props: missing name close: "+closeName);
+            _logger.LogWarning(this, "parse include props: missing name close: " + closeName);
             return def;
         }
-        y += Include_Part_Postfix.Length+closeName.Length;
+        y += Include_Part_Postfix.Length + closeName.Length;
 
-        var block = tpl.Substring(a, b - a + 1);
+        var x0 = x;
+        var block = tpl.Substring(x0, b - 1 - x0 + 1 - Include_Part_Prefix.Length);
+        ParseNextIncludeProp(block);
 
         tpl = tpl[..x] + tpl[y..];
 
-        return (tpl,name,props);
+        return (tpl, name, props);
     }
 
-    int ParseNextIncludeProp(string tpl,int startIndex)
+    int ParseNextIncludeProp(string tpl, int startIndex = 0)
     {
         var r = Index_NoNext;
         var x = tpl.IndexOf(Include_Part_Prop_Prefix, startIndex);
@@ -116,21 +118,34 @@ public partial class TemplateBuilder
             _logger.LogWarning(this, "parse include prop end sub-block missing: " + tpl[x..]);
             return r;
         }
+        var x2 = x + Include_Part_Prop_Prefix.Length;
+        var propName = tpl.Substring(x2,y-x2);
 
+        var z = tpl.IndexOf(Include_Part_Prop_Prefix, y);
+        if (z < 0)
+        {
+            _logger.LogWarning(this, "parse include prop end sub-block missing: " + tpl[y..]);
+            return r;
+        }
+        r = z;
+        var a = y + Include_Part_Prop_Postfix.Length;
+        var b = z - 1;
+        var propValue = tpl.Substring(a, b - a + 1);
 
         return r;
     }
 
-    (string name,Dictionary<string,object?> props) ExtractProps(string name)
+    (string name, Dictionary<string, object?> props) ExtractProps(string name)
     {
         var props = new Dictionary<string, object?>();
         var def = (name, props);
 
         if (!name.EndsWith(')')) return def;
         var x = name.IndexOf('(');
-        if (x<0) {
-            _logger.LogWarning(this, 
-                "part include properties delcaration error: missing ( in props list: "+name);
+        if (x < 0)
+        {
+            _logger.LogWarning(this,
+                "part include properties delcaration error: missing ( in props list: " + name);
             return def;
         }
         var decl = name[(x + 1)..^1];
@@ -150,7 +165,7 @@ public partial class TemplateBuilder
             props.AddOrReplace(t[0], t[1]);
         }
 
-        return (name,props);
+        return (name, props);
     }
 
     string? GetTemplateFile(string partFile)
