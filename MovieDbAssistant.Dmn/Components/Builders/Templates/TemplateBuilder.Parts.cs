87bd1d517984c7a0b71709dde51e1ff7290b90b1
franck gaspoz <franck.gaspoz@gmail.com>
2024-10-15 15:11:06 +0200

    added tpl parts inline props

--- MovieDbAssistant.Dmn/Components/Builders/Templates/TemplateBuilder.Parts.cs
+++ MovieDbAssistant.Dmn/Components/Builders/Templates/TemplateBuilder.Parts.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 
+using MovieDbAssistant.Dmn.Configuration.Extensions;
 using MovieDbAssistant.Lib.Components.Logger;
+using MovieDbAssistant.Lib.Extensions;
 
 namespace MovieDbAssistant.Dmn.Components.Builders.Templates;
 
@@ -17,15 +19,24 @@ public partial class TemplateBuilder
     
     public const int Index_NoNext = -1;
 
-    string IncludeParts(string tpl)
+    string IncludeParts(string tpl, Dictionary<string, object?>? props = null)
     {
-        var nextPos = 0;
-        while (nextPos > Index_NoNext)
-            (tpl,nextPos) = ParseNextInclude(tpl,nextPos);
+        tpl = ParseIncludes(tpl,props ?? []);
         return tpl;
     }
 
-    (string content,int nextPos) ParseNextInclude(string tpl,int startPos=0)
+    string ParseIncludes(string tpl,Dictionary<string, object?> props)
+    {
+        var nextPos = 0;
+        while (nextPos > Index_NoNext)
+            (tpl, nextPos) = ParseNextInclude(tpl, props, nextPos);
+        return tpl;
+    }
+
+    (string content,int nextPos) ParseNextInclude(
+        string tpl,
+        Dictionary<string, object?> props,
+        int startPos=0)
     {
         var x = tpl.IndexOf(Include_Part_Prefix, startPos);
         if (x < 0) return (tpl, Index_NoNext);
@@ -35,22 +46,56 @@ public partial class TemplateBuilder
         var a = x + Include_Part_Postfix.Length;
         var b = y - 1;
         var name = tpl.Substring(a,b-a+1);
-        
+        (name, var tplProps) = ExtractProps(name);
+        tplProps.MergeInto(props);
+
         var partFile = name + Parts_File_Extensions;
         var file = GetTemplateFile(partFile);
         if (file==null) return (tpl, nextY);
 
         var partContent = File.ReadAllText(file);
 
+        // parse part vars
+        partContent = SetVars(partContent, props);
+
         // recurse part
-        var nextPos = 0;
-        while (nextPos > Index_NoNext)
-            (partContent, nextPos) = ParseNextInclude(partContent, nextPos);
+        partContent = ParseIncludes(partContent, props);
 
         var left = tpl[..x];
         var right = tpl[nextY..];
         tpl = left + partContent + right;
         return (tpl, nextY);
+    }
+
+    (string name,Dictionary<string,object?> props) ExtractProps(string name)
+    {
+        var props = new Dictionary<string, object?>();
+        var def = (name, props);
+        if (!name.EndsWith(')')) return def;
+        var x = name.IndexOf('(');
+        if (x<0) {
+            _logger.LogWarning(this, 
+                "part include properties delcaration error: missing ( in props list: "+name);
+            return def;
+        }
+        var decl = name[(x + 1)..^1];
+        name = name[..x];
+        var decls = decl.Split(',');
+        foreach (var pdecl in decls)
+        {
+            var t = pdecl.Split('=');
+            if (t.Length != 2)
+            {
+                _logger.LogWarning(this,
+                    "part include properties declaration error: attempted <key> = <value> but found: "
+                    + decl
+                    );
+                return def;
+            }
+            props.AddOrReplace(t[0], t[1]);
+        }
+
+        return (name,props);
     }
 
     string? GetTemplateFile(string partFile)
@@ -63,13 +108,7 @@ public partial class TemplateBuilder
         var file = Path.Combine(tplPartsPath, partFile);
         if (File.Exists(file)) return file;
 
-        var rscPartsPath = Path.Combine(
-            Directory.GetCurrentDirectory(),
-            _dmnSettings.Value.Paths.Resources,
-            _dmnSettings.Value.Paths.RscHtml,
-            _dmnSettings.Value.Paths.RscHtmlAssets,
-            _dmnSettings.Value.Paths.RscHtmlAssetsTpl
-            );
+        var rscPartsPath = _dmnSettings.Value.EngineTpsPath();
         file = Path.Combine(rscPartsPath, partFile);
         if (File.Exists(file)) return file;
 
--- MovieDbAssistant.Lib/Extensions/CollectionExtensions.cs
+++ MovieDbAssistant.Lib/Extensions/CollectionExtensions.cs
@@ -47,4 +47,39 @@ public static class CollectionExtensions
         t!.AddRange(add);
         return t!.Distinct().ToList();
     }
+
+    /// <summary>
+    /// add or replace avalue in an array
+    /// </summary>
+    /// <typeparam name="TKey">keys type</typeparam>
+    /// <typeparam name="TValue">values type</typeparam>
+    /// <param name="dic"></param>
+    /// <param name="value"></param>
+    public static void AddOrReplace<TKey,TValue>(
+        this Dictionary<TKey,TValue> dic,
+        TKey key,
+        TValue value)
+        where TKey: notnull
+    {
+        if (!dic.ContainsKey(key))
+            dic.Add(key, value);
+        else
+            dic[key] = value;
+    }
+
+    /// <summary>
+    /// Merges the dic into the dic
+    /// </summary>
+    /// <typeparam name="TKey"/>
+    /// <typeparam name="TValue"/>
+    /// <param name="src">The src.</param>
+    /// <param name="target">The target.</param>
+    public static void MergeInto<TKey,TValue>(
+        this Dictionary<TKey,TValue> src,
+        Dictionary<TKey,TValue> target)
+        where TKey: notnull
+    {
+        foreach (var kvp in src)
+            target.AddOrReplace(kvp.Key, kvp.Value);
+    }
 }
