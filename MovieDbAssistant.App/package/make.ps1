eab232dc2985adb2a4200070ffe1ef184ed2b3cf
franck gaspoz <franck.gaspoz@gmail.com>
2024-11-05 14:40:08 +0100

    add notes + fix make appx

---
+++ MovieDbAssistant.App/package/make.ps1
@@ -0,0 +1,10 @@
+﻿#-----------------------------------------------
+# make appx: copy publish files + refister appx
+#-----------------------------------------------
+
+Get-AppxPackage -publisher "CN=Franck Gaspoz Software, O=Franck Gaspoz Corporation, C=US"
+Remove-AppxPackage MovieDbAssistant_1.0.0.0_x64__xtrrbsjxvn07w
+
+xcopy ..\bin\Release\net8.0-windows10.0.22621.0\win-x64\ . /Q /R /Y
+
+add-appxpackage –register AppxManifest.xml
