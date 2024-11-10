67d974b72eb9748c1417755b92d70279271950fe
franck gaspoz <franck.gaspoz@gmail.com>
2024-11-10 22:47:17 +0100

    improve scrap test script

--- MovieDbAssistant.App/tools/scrap-imdb.ps1
+++ MovieDbAssistant.App/tools/scrap-imdb.ps1
@@ -1,13 +1,15 @@
 # movie-db-scrapper-windows-64bit-intel-1.1.1.exe %*
 # example:
-# cls ; .\scrap-imdb.ps1 "Conjuring : Sous l'emprise du Diable" "release_date=2021-01-01,2021-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&count=10,country_of_origin=FR"
+# cls ; .\scrap-imdb.ps1 "Conjuring : Sous l'emprise du Diable" "release_date=2019-01-01,2019-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&count=10,country_of_origin=FR"
 
-param([string]$title="",[string]$filters="") 
+param([string]$title="",[string]$year="",[string]$country="") 
 
 cls
-echo $title
-echo $filters
+echo title=$title
+echo year=$year
+echo country=$country
 #pause
-del ./test.json
-./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title $filters
+#del ./test.json
+#./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title $filters
+./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title "release_date=$year-01-01,$year-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&count=10,country_of_origin=$country"
 code ./test.json
