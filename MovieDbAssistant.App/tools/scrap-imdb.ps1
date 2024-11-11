6e24f4c49f95b779f71b67cacb1db2cf2541b882
franck gaspoz <franck.gaspoz@gmail.com>
2024-11-11 19:52:03 +0100

    fix test case and test script

--- MovieDbAssistant.App/tools/scrap-imdb.ps1
+++ MovieDbAssistant.App/tools/scrap-imdb.ps1
@@ -12,5 +12,5 @@ echo country=$country
 #del ./test.json
 #ex: ./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json "Le marchand de sable" "release_date=2022-01-01,2022-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&user-rating=6.2,6.2&country_of_origin=FR&count=10"
 #./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title $filters
-./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title "release_date=$year-01-01,$year-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&count=10&country_of_origin=$country&user_rating=6.2,6.2"
+./movie-db-scrapper-windows-64bit-intel-1.1.1.exe imdb test.json $title "release_date=$year-01-01,$year-12-31&title_type=feature,tv_series,tv_miniseries,video,tv_special,tv_movie,tv_episode&count=10&country_of_origin=$country"
 code ./test.json
