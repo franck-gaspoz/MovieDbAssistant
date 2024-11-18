bbd214e4219b4914a95606d1bd2dedc10df8c8ef
franck gaspoz <franck.gaspoz@gmail.com>
2024-11-18 18:37:09 +0100

    update media source model

--- MovieDbAssistant.Dmn/Models/Scrap/Json/MediaSource.cs
+++ MovieDbAssistant.Dmn/Models/Scrap/Json/MediaSource.cs
@@ -20,5 +20,25 @@ public sealed partial class MediaSource
     /// </summary>
     public string? MediaPhysicalTypeId { get; set; }
 
+    /// <summary>
+    /// require VPN
+    /// </summary>
+    public bool RequireVPN { get; set; }
+
+    /// <summary>
+    /// indicates if the provider insert adds in media (free with adds)s
+    /// </summary>
+    public bool WithAds { get; set; }
+
+    /// <summary>
+    /// geo restricted (2 digits country codes list)
+    /// </summary>
+    public List<string> GeoRestricted { get; set; } = [];
+
+    /// <summary>
+    /// ui embeded player name/ref if any
+    /// </summary>
+    public string? Embed { get; set; }
+
     public MediaSource(string? path = null) => Path = path;
 }
