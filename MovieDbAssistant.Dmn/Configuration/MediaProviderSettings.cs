6f387435337bad9b0b424a7933a40cccf97a6dca
franck gaspoz <franck.gaspoz@gmail.com>
2024-11-18 19:03:37 +0100

    update the right model

--- MovieDbAssistant.Dmn/Configuration/MediaProviderSettings.cs
+++ MovieDbAssistant.Dmn/Configuration/MediaProviderSettings.cs
@@ -14,4 +14,24 @@ public sealed class MediaProviderSettings
     /// paths masks
     /// </summary>
     public required string[] Paths { get; set; }
+
+    /// <summary>
+    /// require VPN
+    /// </summary>
+    public bool RequireVPN { get; set; }
+
+    /// <summary>
+    /// indicates if the provider insert adds in media (free with adds)s
+    /// </summary>
+    public bool WithAds { get; set; }
+
+    /// <summary>
+    /// geo restricted (2 digits country codes list)
+    /// </summary>
+    public List<string> GeoRestricted { get; set; } = [];
+
+    /// <summary>
+    /// ui embeded player name/ref if any
+    /// </summary>
+    public string? Embed { get; set; }
 }
